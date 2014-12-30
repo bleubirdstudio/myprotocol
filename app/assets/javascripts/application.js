571428b96231d7476cd6aac979b89f768d28caa4
David Hunt <davidhunt@Davids-iMac.local>
2014-12-30 00:10:32 -0500

    done for the night

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,9 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require bootstrap.min
 //= require_tree .
+
+$('#myModal').on('shown.bs.modal', function () {
+	$('#myInput').focus()
+})
